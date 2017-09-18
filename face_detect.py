3fd80f9d261f405dde5f214819aa19e622cbce5c
Kothari <sumit.kothari>
2017-09-18 19:15:15 +0530

    added real time webcam feed

--- face_detect.py
+++ face_detect.py
@@ -2,30 +2,57 @@ import cv2
 import sys
 
 # Get user supplied values
-imagePath = sys.argv[1]
-cascPath = sys.argv[2]
+# imagePath = sys.argv[1]
+cascPath = "haarcascade_frontalface_default.xml"
 
 # Create the haar cascade
 faceCascade = cv2.CascadeClassifier(cascPath)
 
-# Read the image
-image = cv2.imread(imagePath)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+video_capture = cv2.VideoCapture(0)
 
-# Detect faces in the image
-faces = faceCascade.detectMultiScale(
-    gray,
-    scaleFactor=1.1,
-    minNeighbors=5,
-    minSize=(30, 30)
-    #flags = cv2.CV_HAAR_SCALE_IMAGE
-)
+while True:
+    ret, image = video_capture.read()
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-print "Found {0} faces!".format(len(faces))
+    # Detect faces in the image
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30)
+        #flags = cv2.CV_HAAR_SCALE_IMAGE
+    )
 
-# Draw a rectangle around the faces
-for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    print("Found {0} faces!".format(len(faces)))
 
-cv2.imshow("Faces found", image)
-cv2.waitKey(0)
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    cv2.imshow("Faces found", image)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+# # Read the image
+# image = cv2.imread(imagePath)
+# gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# # Detect faces in the image
+# faces = faceCascade.detectMultiScale(
+#     gray,
+#     scaleFactor=1.1,
+#     minNeighbors=5,
+#     minSize=(30, 30)
+#     #flags = cv2.CV_HAAR_SCALE_IMAGE
+# )
+
+# print "Found {0} faces!".format(len(faces))
+
+# # Draw a rectangle around the faces
+# for (x, y, w, h) in faces:
+#     cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+# cv2.imshow("Faces found", image)
+# cv2.waitKey(0)
