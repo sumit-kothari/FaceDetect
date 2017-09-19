b76dcd36f0cfb8828721eff1f19f4c40b1d1f61a
Kothari <sumit.kothari>
2017-09-19 19:15:42 +0530

    flask app multithreading added

--- camera.py
+++ camera.py
@@ -1,21 +1,32 @@
 import cv2
+import threading
 
 cascPath = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascPath)
 
 
-class VideoCamera(object):
+facesBox = None
+gray = None
+
+
+class MyThread(threading.Thread):
 
     def __init__(self):
-        self.video = cv2.VideoCapture(0)
+        threading.Thread.__init__(self)
+        self.stopped = False
 
-    def __del__(self):
-        self.video.release()
+    def run(self):
+        global facesBox
 
-    def get_frame(self):
-        success, image = self.video.read()
+        while (~(gray is None)):
+            if self.stopped:
+                return
 
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            facesBox = self.predict(gray)
+
+    def predict(self, gray):
+        if self.stopped:
+            return
 
         # Detect faces in the image
         faces = faceCascade.detectMultiScale(
@@ -27,9 +38,33 @@ class VideoCamera(object):
 
         print("Found {0} faces!".format(len(faces)))
 
-        # Draw a rectangle around the faces
-        for (x, y, w, h) in faces:
-            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        return faces
+
+    def stop(self):
+        self.stopped = True
+
+
+class VideoCamera(object):
+
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+        self.this_thread = MyThread()
+        self.this_thread.start()
+
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self):
+        success, image = self.video.read()
+
+        global gray
+
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        if facesBox is not None:
+            # Draw a rectangle around the faces
+            for (x, y, w, h) in facesBox:
+                cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
         ret, jpeg = cv2.imencode('.jpg', image)
         return jpeg.tobytes()
